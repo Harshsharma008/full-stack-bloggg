91423c3f5e8dad7e59c5d8e734578353c1cd3068
Harshsharma008 <harshsharma01012002@gmail.com>
2024-10-17 22:20:56 +0530

    blog project

---
+++ backend/config/plugins.ts
@@ -0,0 +1,21 @@
+export default () => ({
+    // Other plugin configurations can go here
+    'ckeditor': {
+        enabled: true,
+        config: {
+            // CKEditor configuration options
+            // You can customize it based on your requirements
+            toolbar: [
+                'heading', '|',
+                'bold', 'italic', 'link', '|',
+                'bulletedList', 'numberedList', '|',
+                'blockQuote', 'insertTable', '|',
+                'undo', 'redo'
+            ],
+            seo: {
+                enabled: true,
+            },
+
+        },
+    },
+});
---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,62 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+    description: '';
+  };
+  attributes: {
+    metaTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 50;
+        maxLength: 160;
+      }>;
+    metaImage: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    metaSocial: Schema.Attribute.Component<'shared.meta-social', true>;
+    keywords: Schema.Attribute.Text;
+    metaRobots: Schema.Attribute.String;
+    structuredData: Schema.Attribute.JSON;
+    metaViewport: Schema.Attribute.String;
+    metaDetails: Schema.Attribute.Component<'shared.meta-social', true>;
+  };
+}
+
+export interface SharedMetaSocial extends Struct.ComponentSchema {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    socialNetwork: Schema.Attribute.Enumeration<['Facebook', 'Twitter']> &
+      Schema.Attribute.Required;
+    title: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    description: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.seo': SharedSeo;
+      'shared.meta-social': SharedMetaSocial;
+    }
+  }
+}
